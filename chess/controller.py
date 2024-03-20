71cdc3d43ac02be7aaad072327f70ed33170c3d3
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-03-20 18:02:31 +0100

    finish match generation

--- chess/controller.py
+++ chess/controller.py
@@ -1,5 +1,6 @@
 import random
 from datetime import date
+from operator import itemgetter
 
 from chess.model import Player, Tournament, Round
 from chess.generator import Creator
@@ -13,7 +14,6 @@ class Controller():
     def run(self):
         # afficher le menu principal
 
-        # ajouter décorateur avec le Creator
         tournoi1 = self.manage_tournament.create(name="Tournoi en arène",
                                                  start_date="2054-01-01",
                                                  end_date="2054-05-01",
@@ -37,13 +37,16 @@ class Controller():
                                             birthdate="1988-05-15",
                                             national_chess_id="MM456",
                                             )
+
+        # génération aléatoire de joueurs TODO : passer Creator en tant que décorateur
         createur = Creator()
         player_list = createur.player(10)
+
+        # on ajoute les joueurs au tournoi
         for player in player_list:
             self.manage_tournament.add_player(tournoi1, player)
-        # self.manage_tournament.add_player(tournoi1, player1)
-        # self.manage_tournament.add_player(tournoi1, player2)
 
+        # on génère l'ensemble des matchs
         self.manage_tournament.manage_round.create_matchs(tournoi1)
 
 
@@ -92,7 +95,10 @@ class TournamentManagement():
         return tournament
 
     def add_player(self, tournament: Tournament, player: Player):
-        tournament.player_list.append(player)
+        # initialisation du score à 0
+        score = 0
+        tournament.player_list.append([player, score])
+        # tournament.player_list.append((player, score))
         PlayerManagement.add_tournament(player, tournament)
 
     def __create_round(self, tournament: Tournament, nb_of_rounds: int):
@@ -124,30 +130,38 @@ class RoundManagement():
         return round
 
     def create_matchs(self, tournament: Tournament):
-        # création du nombre de matchs en fonction du nombre de joueurs
 
-
-        # au premier tour random sur les paires
-        # random.choice(liste_first_round)
-        liste_first_round = tournament.player_list
-        while len(liste_first_round) >= 2:
-            player1 = liste_first_round.pop(random.randrange(len(liste_first_round)))
-            player2 = liste_first_round.pop(random.randrange(len(liste_first_round)))
-        # génération d'un score aléatoire
-            result_match = self.match_add_scores(player1, player2)
-            tournament.rounds_list[0].matchs_list.append(result_match)
-
-        # classement de fin de tour
-            result_match.sort(key=itemgetter(2), reverse=True)
-
-        # second tour (et suivants)
-
-
-#        tournament.player_list
-        #x.pop(random.randint(0,len(x)-1))
+        for round_number in range(tournament.number_of_rounds):
+            # on génère des couples de joueurs et un score aléatoire
+            self.create_players_peers(tournament, round_number)
+            # on tri la liste des joueurs suivant les résultats
+            tournament.player_list.sort(key=itemgetter(1), reverse=True)
         pass
-        # round 2 à x
-        # faire jouer celui qui n'a pas joué au tour 1 si nb impaire ?
+
+
+    def create_players_peers(self, tournament: Tournament, round_number: int) -> None:
+
+        actual_round_list = tournament.player_list.copy()
+
+        while len(actual_round_list) >= 2:
+
+            # au premier tour random sur les paires
+            if round_number == 0:  # le premier round est celui d'indice 0
+                player1 = actual_round_list.pop(random.randrange(len(actual_round_list)))
+                player2 = actual_round_list.pop(random.randrange(len(actual_round_list)))
+            else:
+            # ensuite on prend suivant l'ordre du classement
+                player1 = actual_round_list.pop(0)
+                player2 = actual_round_list.pop(0)
+
+            # génération d'un score aléatoire
+            result_match = self.match_add_scores(player1, player2)
+            tournament.rounds_list[round_number].matchs_list.append(result_match)
+
+            # mise à jour des scores généraux du tournoi
+            tournament.player_list[tournament.player_list.index(player1)][1] += result_match[0][1]
+            tournament.player_list[tournament.player_list.index(player2)][1] += result_match[1][1]
+
 
 
     def match_add_scores(self, player1: Player, player2: Player) -> tuple:
