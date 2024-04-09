4c697e7f3e692128883f971efd82f16f2c9c7aa8
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-04-09 08:34:11 +0200

    add class InputErrorHandler

--- chess/view.py
+++ chess/view.py
@@ -17,18 +17,18 @@ from chess.command import (MainMenuCommand,
                            CreateRoundCommand,
                            )
 
-# # exemple de décorateur a généraliser pour get_user_choice()
-# class InputErrorHandler:
-#     @staticmethod
-#     def catch_input_errors(func):
-#         def wrapper(*args, **kwargs):
-#             while True:
-#                 try:
-#                     return func(*args, **kwargs)
-#                 except Exception as e:
-#                     print(e)
-#         return wrapper
-#         logging.debug("choix menu >> " + str(args[choix - 1]))
+
+class InputErrorHandler:
+    @staticmethod
+    def catch_input_errors(func):
+        def wrapper(*args, **kwargs):
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(e)
+        return wrapper
+
 
 # class MainView(InputErrorHandler):
 #     @InputErrorHandler.catch_input_errors
@@ -37,164 +37,138 @@ from chess.command import (MainMenuCommand,
 #         return TournamentMenuCommand(choice)
 
 
-class MainView:
+class MainView(InputErrorHandler):
     def __init__(self):
         self.main_menu()
 
     def main_menu(self):
         print(text.MAIN_MENU)
 
+    @InputErrorHandler.catch_input_errors
     def get_user_choice(self):
-        while True:
-            try:
-                choice = input()
-                return MainMenuCommand(choice)
-            except Exception as e:
-                print(e)
+        choice = input()
+        return MainMenuCommand(choice)
 
 
-class PlayerView:
+class PlayerView(InputErrorHandler):
     def __init__(self) -> None:
         pass
 
     def player_menu(self):
         print(text.PLAYER_MENU)
 
+    @InputErrorHandler.catch_input_errors
     def get_user_choice(self):
-        while True:
-            try:
-                choice = input()
-                return PlayerMenuCommand(choice)
-            except Exception as e:
-                print(e)
+        choice = input()
+        return PlayerMenuCommand(choice)
 
     def display_players(self, players_list):
         print(players_list)
 
+    @InputErrorHandler.catch_input_errors
     def player_create_menu(self):
         print(text.PLAYER_CREATE_MENU)
-        while True:
-            try:
-                last_name = input("Saisir le nom de famille :")
-                first_name = input("Saisir le prénom :")
-                birthdate = input("Saisir la date de naissance au format AAAA-MM-JJ :")
+        last_name = input("Saisir le nom de famille :")
+        first_name = input("Saisir le prénom :")
+        birthdate = input("Saisir la date de naissance au format AAAA-MM-JJ :")
 
-                command = PlayerCreateCommand(last_name=last_name, first_name=first_name, birthdate=birthdate)
-                return command
-            except Exception as e:
-                print(e)
+        command = PlayerCreateCommand(last_name=last_name, first_name=first_name, birthdate=birthdate)
+        return command
 
+    @InputErrorHandler.catch_input_errors
     def player_generate_menu(self):
-        while True:
-            try:
-                print(text.PLAYER_GENERATE_MENU)
-                choice = input()
-                return PlayerGenerateCommand(choice)
-            except Exception as e:
-                print(e)
+        print(text.PLAYER_GENERATE_MENU)
+        choice = input()
+        return PlayerGenerateCommand(choice)
 
 
-class TournamentView:
+class TournamentView(InputErrorHandler):
     def __init__(self) -> None:
         pass
 
     def tournament_menu(self):
         print(text.TOURNAMENT_MENU)
 
+    @InputErrorHandler.catch_input_errors
     def tournament_menu_user_choice(self):
-        while True:
-            try:
-                choice = input()
-                return TournamentMenuCommand(choice)
-            except Exception as e:
-                print(e)
+        choice = input()
+        return TournamentMenuCommand(choice)
 
     def display_tournaments(self, tournaments_list):
         print(tournaments_list)
 
+    @InputErrorHandler.catch_input_errors
     def tournament_details_get_tournament_id(self):
+        # TODO sortir le print du décorateur
         print(text.TOURNAMENT_DETAIL_MENU)
-        while True:
-            try:
-                tournament_id = input()
-                return TournamentDetailCommand(tournament_id)
-            except Exception as e:
-                print(e)
+        tournament_id = input()
+        return TournamentDetailCommand(tournament_id)
 
     def display_tournament_details(self, text: str):
+        # TODO
         print(text)
 
+    @InputErrorHandler.catch_input_errors
     def tournament_create_menu(self):
+        # TODO sortir le print du décorateur
         print(text.TOURNAMENT_CREATE_MENU)
-        while True:
-            try:
-                name = input("Saisir le nom du tournoi :")
-                location = input("Saisir le lieu du tournoi :")
-                start_date = input("Saisir la date de début (format AAAA-MM-JJ) :")
-                end_date = input("Saisir la date de fin (format AAAA-MM-JJ) :")
-                number_of_rounds = input("Saisir le nombre de tours (4 par défaut):")
-                description = input("Saisir la description (facultatif) :")
 
-                command = TournamentCreateCommand(name=name,
-                                                  location=location,
-                                                  start_date=start_date,
-                                                  end_date=end_date,
-                                                  number_of_rounds=number_of_rounds,
-                                                  description=description,
-                                                  )
-                return command
-            except Exception as e:
-                print(e)
+        name = input("Saisir le nom du tournoi :")
+        location = input("Saisir le lieu du tournoi :")
+        start_date = input("Saisir la date de début (format AAAA-MM-JJ) :")
+        end_date = input("Saisir la date de fin (format AAAA-MM-JJ) :")
+        number_of_rounds = input("Saisir le nombre de tours (4 par défaut):")
+        description = input("Saisir la description (facultatif) :")
 
+        command = TournamentCreateCommand(name=name,
+                                            location=location,
+                                            start_date=start_date,
+                                            end_date=end_date,
+                                            number_of_rounds=number_of_rounds,
+                                            description=description,
+                                            )
+        return command
+
+    @InputErrorHandler.catch_input_errors
     def tournament_generate_menu(self):
         print(text.TOURNAMENT_GENERATE_MENU)
-        while True:
-            try:
-                tournament_number = input()
-                return TournamentGenerateCommand(tournament_number)
-            except Exception as e:
-                print(e)
+        tournament_number = input()
+        return TournamentGenerateCommand(tournament_number)
 
+    @InputErrorHandler.catch_input_errors
     def tournament_add_players_get_tournament_id(self):
         print(text.TOURNAMENT_ADD_PLAYERS_MENU)
-        while True:
-            try:
-                tournament_id = input()
-                return TournamentAddPlayerCommand(tournament_id)
-            except Exception as e:
-                print(e)
+        tournament_id = input()
+        return TournamentAddPlayerCommand(tournament_id)
 
+    @InputErrorHandler.catch_input_errors
     def tournament_add_players_get_players_id(self):
         print(text.TOURNAMENT_ADD_PLAYERS_LIST)
-        while True:
-            try:
-                players_id = input()
-                return PlayersToAddInTournamentCommand(players_id)
-            except Exception as e:
-                print(e)
+        players_id = input()
+        return PlayersToAddInTournamentCommand(players_id)
+
 
     def tournament_add_players_display(self):
         #TODO
         pass
 
+    @InputErrorHandler.catch_input_errors
     def tournament_menu_start(self):
         print(text.TOURNAMENT_START_MENU)
-        while True:
-            try:
-                choice = input()
-                return TournamentStartCommand(choice)
-            except Exception as e:
-                print(e)
+        choice = input()
+        return TournamentStartCommand(choice)
 
 
-class RoundView:
+class RoundView(InputErrorHandler):
     def __init__(self):
         pass
 
+    @InputErrorHandler.catch_input_errors
     def create_round_menu(self):
         print(text.ROUND_MENU)
         tournament_choice = input()
         return CreateRoundCommand(tournament_choice)
+
 
 
 class View:
